5836b3c16191e132673d1574c1b1a8616962b0f7
LuDa5 <133536632+LuDa5@users.noreply.github.com>
2024-11-27 10:26:55 +0200

    switch

--- src/js/header.js
+++ src/js/header.js
@@ -1,17 +1,21 @@
 const burgerBtn = document.querySelector('.burger-btn'); // Кнопка бургер
 const modalOverlay = document.querySelector('.modal-overlay'); // Модалка
 const closeModalBtn = modalOverlay.querySelector('.icon-close'); // Крестик в модалке
+const switchInput = document.querySelector('.switch-input');
+const wrappArrow = document.querySelector('.wrapp-arrow');
 
 // Открыть модалку
 burgerBtn.addEventListener('click', () => {
   modalOverlay.classList.add('active'); // Добавляем класс для отображения модалки
   // burgerBtn.style.display = 'none';
+  wrappArrow.style.display = 'none';
 });
 
 // Закрыть модалку
 closeModalBtn.addEventListener('click', () => {
   modalOverlay.classList.remove('active'); // Убираем класс для скрытия модалки
   // burgerBtn.style.display = 'flex';
+  wrappArrow.style.display = 'flex';
 });
 
 // Закрытие модалки по клику вне её области
@@ -19,4 +23,16 @@ modalOverlay.addEventListener('click', event => {
   if (event.target === modalOverlay) {
     modalOverlay.classList.remove('active');
   }
+});
+
+document.addEventListener('DOMContentLoaded', () => {
+  const isDarkMode = localStorage.getItem('dark-theme') === 'true';
+  switchInput.checked = isDarkMode;
+  document.body.classList.toggle('dark-theme', isDarkMode);
+});
+
+switchInput.addEventListener('change', () => {
+  const isChecked = switchInput.checked;
+  document.body.classList.toggle('dark-theme', isChecked);
+  localStorage.setItem('dark-theme', isChecked);
 });
